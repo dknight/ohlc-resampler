21cf701812f27b07e5bec8f19a32675ccc483cd3
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2022-11-04 10:36:23 +0200

    Added temporary binaries for different archs.

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compiles ohlc-resampler for different arhitectures.
+
+BINDIR="./bin"
+
+GOOS=linux GOARCH=amd64 go build -o "$BINDIR/ohlc-resampler-amd64-linux"
+GOOS=darwin GOARCH=amd64 go build -o "$BINDIR/ohlc-resampler-amd64-darwin"
+GOOS=darwin GOARCH=arm64 go build -o "$BINDIR/ohlc-resampler-arm64-darwin"
+GOOS=windows GOARCH=amd64 go build -o "$BINDIR/ohlc-resampler.exe"
