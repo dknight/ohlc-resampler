e4b1e9c6330d1c1293b9e5bb82844dda749e66e0
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2022-11-12 18:53:59 +0200

    Removed binaries.

--- compile.sh
+++ compile.sh
@@ -4,6 +4,8 @@
 
 BINDIR="./bin"
 
+mkdir -pv "$BINDIR"
+
 GOOS=linux GOARCH=amd64 go build -o "$BINDIR/ohlc-resampler-amd64-linux"
 GOOS=darwin GOARCH=amd64 go build -o "$BINDIR/ohlc-resampler-amd64-darwin"
 GOOS=darwin GOARCH=arm64 go build -o "$BINDIR/ohlc-resampler-arm64-darwin"
